7bead3335e21e70996f441c5fe8754dc90d2e564
Vivek Chakravorty <77537702+WitteDuivel@users.noreply.github.com>
2021-03-17 20:42:05 +0530

    Update BOOK1.py

--- BOOK1.py
+++ BOOK1.py
@@ -1,37 +1,53 @@
 import mysql.connector
-mydb= mysql.connector.connect(host="127.0.0.1", user="root", passwd="2zkNKcz&EOZaRjc$",database="library_management_project")
+mydb= mysql.connector.connect(host="localhost", user="root", passwd="2zkNKcz&EOZaRjc$",database="library_management_project")
 mycursor=mydb.cursor()
 def bookadd():
-    bookIDp=input("ENTER THE BOOK'S ID:- ")
-    #todo query to add this expression into the database
+    bookIDp=input("ENTER THE BOOK'S ID:- ") 
+    mycursor.execute("INSERT INTO BOOK (book_ID) VALUES (bookIDp)")
+    mydb.commit()
     bktitlep=input("ENTER THE BOOK'S TITLE/NAME:- ")
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO BOOK (bktitle) VALUES (bktitlep)")
+    mydb.commit()
     bkauthorp=input("ENTER THE BOOK'S AUTHOR NAME:- ")
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO BOOK (bkauthor) VALUES (bkauthorp)")
+    mydb.commit()
     bkpublisherp=input("ENTER THE BOOK'S PUBLISHER NAME:- ")
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO BOOK (bkpubisher) VALUES (bkpublisherp)")
+    mydb.commit()
     bkcopiesp=int(input("ENTER THE TOTAL NUMBER OF THE COPIES OF THIS BOOK:- "))
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO BOOK (bkcopies) VALUES (bkopiesp)")
+    mydb.commit()
     bk_sourcep=input("ENTER THE BOOK'S SOURCE:- ")
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO BOOK (bk_source) VALUES (bk_sourcep)")
+    mydb.commit()
     bk_costp=int(input("ENTER THE BOOK'S COST OF ONE UNIT:- "))
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO BOOK (bk_cost) VALUES (bk_costp)")
+    mydb.commit()
     bk_remarksp=input("ENTER BOOK'S REMARKS:- ")
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO BOOK (bk_remarks) VALUES (bk_remarksp)")
+    mydb.commit()
 def bookupdate():
     bookIDp=input("ENTER THE BOOK'S ID:- ")
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE BOOK SET book_ID = bookIDp")
+    mydb.commit()
     bktitlep=input("ENTER THE BOOK'S TITLE/NAME:- ")
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE USERS SET bktitles = bktitlesp")
+    mydb.commit()
     bkauthorp=input("ENTER THE BOOK'S AUTHOR NAME:- ")
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE BOOK SET bkauthor = bkauthorp")
+    mydb.commit()
     bkpublisherp=input("ENTER THE BOOK'S PUBLISHER NAME:- ")
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE BOOK SET bkpublisher = bkpublisherp")
+    mydb.commit()
     bkcopiesp=int(input("ENTER THE TOTAL NUMBER OF THE COPIES OF THIS BOOK:- "))
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE BOOK SET bkcopies = bkcopiesp")
+    mydb.commit()
     bk_sourcep=input("ENTER THE BOOK'S SOURCE:- ")
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE BOOK SET bk_source = bk_sourcep")
+    mydb.commit()
     bk_costp=int(input("ENTER THE BOOK'S COST OF ONE UNIT:- "))
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE BOOK SET bk_cost = bk_costp")
+    mydb.commit()
     bk_remarksp=input("ENTER BOOK'S REMARKS:- ")
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE BOOK SET bk_remarks = bk_remarksp")
+    mydb.commit()
