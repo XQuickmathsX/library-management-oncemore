ec6cbe6ea2ff848cf7732c4e481c7eb1d6234b21
Vivek Chakravorty <77537702+WitteDuivel@users.noreply.github.com>
2021-03-17 20:41:09 +0530

    Update STUDENTS1.py

--- STUDENTS1.py
+++ STUDENTS1.py
@@ -1,41 +1,59 @@
 import mysql.connector
-mydb= mysql.connector(host="127.0.0.1", user="root", passwd="2zkNKcz&EOZaRjc$",database="library_management_project")
+mydb= mysql.connector.connect(host="localhost", user="root", passwd="2zkNKcz&EOZaRjc$",database="library_management_project")
 mycursor=mydb.cursor()
 def useradd():
     studIDp=int(input("ENTER YOUR STUDENT_ID:- "))
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO STUDENTS (stud_ID) VALUES (studIDp)")
+    mydb.commit()
     stfnamep=input("ENTER YOUR FIRST NAME:- ")
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO STUDENTS (stfname) VALUES (stfnamep)")
+    mydb.commit()
     stlnamep=input("ENTER YOUR LAST NAME:- ")
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO STUDENTS (stlname) VALUES (stlnamep)")
+    mydb.commit()
     stcoursep=input("ENTER YOUR COURSE:- ")
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO STUDENTS (stcourse) VALUES (stcoursep)")
+    mydb.commit()
     styearp=int(input("ENTER YOUR ADMISSION YEAR:- "))
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO STUDENTS (styear) VALUES (styearp)")
+    mydb.commit()
     stcontactp=int(input("ENTER YOUR CONTACT NUMBER:- "))
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO STUDENTS (stcontact) VALUES (stcontactp)")
+    mydb.commit()
     stagep=int(input("ENTER YOUR AGE:- "))
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO STUDENTS (stage) VALUES (stagep)")
+    mydb.commit()
     stbirthdatep=int(input("ENTER YOUR DATE OF BIRTH(MMDDYYYY):- "))
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO STUDENTS (stbirthdate) VALUES (stbirthdatep)")
+    mydb.commit()
     stgenderp=input("ENTER YOUR GENDER:- ")
-    #todo query to add this expression into the database
+    mycursor.execute("INSERT INTO STUDENTS (stgender) VALUES (stgenderp)")
+    mydb.commit()
 def userupdate():
     studIDp=int(input("ENTER YOUR STUDENT_ID:- "))
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE STUDENTS SET stud_ID=studIDp")
+    mydb.commit()
     stfnamep=input("ENTER YOUR FIRST NAME:- ")
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE STUDENTS SET stud_ID=stfnamep")
+    mydb.commit()
     stlnamep=input("ENTER YOUR LAST NAME:- ")
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE STUDENTS SET stlname=stlnamep")
+    mydb.commit()
     stcoursep=input("ENTER YOUR COURSE:- ")
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE STUDENTS SET stcourse=stcoursep")
+    mydb.commit()
     styearp=int(input("ENTER YOUR ADMISSION YEAR:- "))
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE STUDENTS SET styear=styearp")
+    mydb.commit()
     stcontactp=int(input("ENTER YOUR CONTACT NUMBER:- "))
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE STUDENTS SET stcontract=stcontractp")
+    mydb.commit()
     stagep=int(input("ENTER YOUR AGE:- "))
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE STUDENTS SET stage=stagep")
+    mydb.commit()
     stbirthdatep=int(input("ENTER YOUR DATE OF BIRTH(MMDDYYYY):- "))
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE STUDENTS SET stbirthdate=stbirthdatep")
+    mydb.commit()
     stgenderp=input("ENTER YOUR GENDER:- ")
-    #todo query to update this expression into the database
+    mycursor.execute("UPDATE STUDENTS SET stgender=stgender")
+    mydb.commit()
