b242499ef09f912396b51a64e0f713ea9dd4288a
XQuickmathsX <74904876+XQuickmathsX@users.noreply.github.com>
2021-03-21 18:11:15 +0530

    Update BookReturnRecords1.py

--- BookReturnRecords1.py
+++ BookReturnRecords1.py
@@ -1,36 +1,31 @@
 import mysql.connector
-mydb= mysql.connector(host="127.0.0.1", user="root", passwd="2zkNKcz&EOZaRjc$",database="library_management_project")
+mydb=mysql.connector.connect(host="localhost",user="root",passwd="5678",database="library_management_project")
+mycursor=mydb.cursor()
 def returneestudentadd():
     Borrowers_IDp=int(input("ENTER YOUR BORROWERS_ID:- "))
-    #todo query to add this expression into the database
     bookIDp=int(input("ENTER YOUR BOOK'S ID:- "))
-    #todo query to add this expression into the database
     bktitlep=input("ENTER YOUR BOOK'S TITLE/NAME:- ")
-    #todo query to add this expression into the database
     studIDp=input("ENTER YOUR STUDENT_ID:- ")
-    #todo query to add this expression into the database
     stfnamep=input("ENTER YOUR FIRST NAME:- ")
-    #todo query to add this expression into the database
     releaseDatep=int(input("ENTER RELEASE DATE(MMDDYYYY):- "))
-    #todo query to add this expression into the database
     duedatep=int(input("ENTER YOUR DUE DATE(MMDDYYYY):- "))
-    #todo query to add this expression into the database
     bkdatereturnp=int(input("ENTER THE BOOK RETURN DATE(MMDDYYYY)"))
-    #todo query to add this expression into the database
+    sql = "INSERT INTO BookReturnRecords (Borrowers_ID, book_ID, bktitle, stud_ID, stfname, releaseDate, duedate, bkdatereturn) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (Borrowers_IDp, bookIDp, bktitlep, studIDp, stfnamep, releaseDatep, duedatep, bkdatereturnp)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "was inserted.")
 def returneestaffadd():
     Borrowers_IDp=int(input("ENTER YOUR BORROWERS_ID:- "))
-    #todo query to update this expression into the database
     bookIDp=int(input("ENTER YOUR BOOK'S ID:- "))
-    #todo query to update this expression into the database
     bktitlep=input("ENTER YOUR BOOK'S TITLE/NAME:- ")
-    #todo query to update this expression into the database
     staffIDp=input("ENTER YOUR STUDENT_ID:- ")
-    #todo query to update this expression into the database
     stffnamep=input("ENTER YOUR FIRST NAME:- ")
-    #todo query to update this expression into the database
     releaseDatep=int(input("ENTER RELEASE DATE(MMDDYYYY):- "))
-    #todo query to update this expression into the database
     duedatep=int(input("ENTER YOUR DUE DATE(MMDDYYYY):- "))
-    #todo query to update this expression into the database
     bkdatereturnp=int(input("ENTER THE BOOK RETURN DATE(MMDDYYYY)"))
-    #todo query to update this expression into the database
+    sql = " INSERT INTO BookReturnRecords (Borrowers_ID, book_ID, bktitle, staff_ID, stffname, releaseDate, duedate, bkdatereturn) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (Borrowers_IDp, bookIDp, bktitlep, staffIDp, stffnamep, releaseDatep, duedatep, bkdatereturnp)
+    mycursor.execute(sql,val)
+    mydb.commit()
+    print(mycursor.rowcount, "was inserted.")
